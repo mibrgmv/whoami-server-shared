a5e72be639947a71571c1ea5add0cbd44f407886
mibrgmv <mibrgmv@gmail.com>
2025-07-16 01:18:06 +0300

    fix: config load path

--- config/config.go
+++ config/config.go
@@ -3,23 +3,13 @@ package config
 import (
 	"fmt"
 	"github.com/spf13/viper"
-	"path/filepath"
-	"runtime"
 	"strings"
 )
 
 func LoanConfig(cfg interface{}) error {
-	_, filename, _, ok := runtime.Caller(1)
-	if !ok {
-		return fmt.Errorf("failed to get called information")
-	}
-
-	mainDir := filepath.Dir(filename)
-	configDir := filepath.Join(mainDir, "configs")
-
 	viper.SetConfigName("default")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath(configDir)
+	viper.AddConfigPath("configs")
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
